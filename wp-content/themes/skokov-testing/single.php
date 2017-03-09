947680285bbe81fe622f535ed6eabac81ad8edfb
ahrrhy <ahrrhy@gmail.com>
2017-03-09 10:09:09 +0200

    can't do the related posts

--- wp-content/themes/skokov-testing/single.php
+++ wp-content/themes/skokov-testing/single.php
@@ -10,24 +10,23 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="col-sm-8 site-main" role="main">
+            <main id="main" class="col-sm-8 site-main blog-site-main" role="main">
+                <?php
+                    while ( have_posts() ) : the_post();
+                        get_template_part( 'template-parts/content', get_post_format() );
+                    if (! is_single()){
+                        the_post_navigation();
+                    }
+                    // If comments are open or we have at least one comment, load up the comment template.
+                    if ( comments_open() || get_comments_number() ) :
+                        comments_template();
+                    endif;
+                    endwhile; // End of the loop.
+                ?>
+                <?php dynamic_sidebar( 'share-widget' ); ?>
+                <?php dynamic_sidebar( 'related-widget' ); ?>
+            </main><!-- #main -->
 
-		<?php
-		while ( have_posts() ) : the_post();
-
-			get_template_part( 'template-parts/content', get_post_format() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
--- wp-content/themes/skokov-testing/template-parts/content.php
+++ wp-content/themes/skokov-testing/template-parts/content.php
@@ -13,15 +13,24 @@
 	<header class="entry-header">
 		<?php
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+            the_post_thumbnail('', array( 'src' => $src,
+                'class' => "header-img img-responsive",
+                'alt' => trim(strip_tags( $wp_postmeta->_wp_attachment_image_alt )),));
+			the_title( '<h1 class="entry-title single-article-header"> <span class="header-description arrow_box">', '</span></h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php skokov_testing_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		    <?php if(is_single()) : ?>
+                <div class="entry-meta header-info">
+			        <?php skokov_testing_posted_on(); ?>
+                </div><!-- .entry-meta -->
+            <?php else: ?>
+                <div class="entry-meta">
+                    <?php skokov_testing_posted_on(); ?>
+                </div><!-- .entry-meta -->
+            <?php endif; ?>
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
@@ -34,10 +43,12 @@
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'skokov-testing' ),
-				'after'  => '</div>',
-			) );
+            if(! is_single() ){
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'skokov-testing' ),
+                    'after'  => '</div>',
+                ) );
+            }
 		?>
 	</div><!-- .entry-content -->
 
